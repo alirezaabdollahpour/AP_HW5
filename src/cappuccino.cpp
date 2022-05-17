a2ed38bf67eb96d80a30a5a0a82382402673c620
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-05-17 19:25:52 +0430

    constructor for cappaucino impplement

--- src/cappuccino.cpp
+++ src/cappuccino.cpp
@@ -1,1 +1,20 @@
 #include "cappuccino.h"
+
+Cappuccino::Cappuccino()
+    : side_items {}
+{
+    ingredients.push_back(new Espresso(2));
+    ingredients.push_back(new Milk(2));
+    ingredients.push_back(new MilkFoam(1));
+    name = "Cappuccino";
+}
+
+std::string Cappuccino::get_name()
+{
+    return this->name;
+}
+
+// double Cappuccino::price()
+// {
+
+// }

ad1c083bd367e0694155de613884b606c1f70bad
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-05-21 17:42:03 +0430

    done without brew function

--- src/main.cpp
+++ src/main.cpp
@@ -12,33 +12,49 @@
 #include <ftxui/screen/screen.hpp>
 #include <iostream>
 
-int main(void)
+// int main(void)
+// {
+//     using namespace ftxui;
+
+//     // Define the document
+//     // Element document = hbox({
+//     //     text("left") | border,
+//     //     text("middle") | border | flex,
+//     //     text("right") | border,
+//     // });
+//     Element document = vbox({ text("The window") | bold | color(Color::Blue),
+//         gauge(0.5)
+//             text("The footer") });
+
+//     // Add a border, by calling the `ftxui::border` decorator function.
+//     document = border(document);
+
+//     // Add another border, using the pipe operator.
+//     document = document | border.
+
+//                           // Add another border, using the |= operator.
+//                           document
+//         |= border
+//             Render(screen, document);
+//     screen.Print();
+
+//     return EXIT_SUCCESS;
+// }
+
+int main(int argc, char** argv)
 {
-    using namespace ftxui;
+    if (false) // make false to run unit-tests
+    {
+        // debug section
 
-    // Define the document
-    // Element document = hbox({
-    //     text("left") | border,
-    //     text("middle") | border | flex,
-    //     text("right") | border,
-    // });
-    Element document = vbox({ text("The window") | bold | color(Color::Blue),
-        gauge(0.5)
-            text("The footer") });
-    // Add a border, by calling the `ftxui::border` decorator function.
-    document = border(document);
-
-    // Add another border, using the pipe operator.
-    document = document | border.
-
-                          // Add another border, using the |= operator.
-                          document
-        |= border auto screen = Screen::Create(
-            Dimension::Full(), // Width
-            Dimension::Fit(document) // Height
-        );
-    Render(screen, document);
-    screen.Print();
-
-    return EXIT_SUCCESS;
+    } else {
+        ::testing::InitGoogleTest(&argc, argv);
+        std::cout << "RUNNING TESTS ..." << std::endl;
+        int ret { RUN_ALL_TESTS() };
+        if (!ret)
+            std::cout << "<<<SUCCESS>>>" << std::endl;
+        else
+            std::cout << "FAILED" << std::endl;
+    }
+    return 0;
 }

9707f1612892e88aebf5b73a543f2747622c9447
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-05-22 18:13:30 +0430

    make ingredient beautiful

--- include/ingredient.h
+++ include/ingredient.h
@@ -6,20 +6,10 @@
 
 class Ingredient {
 public:
-    double get_price_unit()
-    {
-        return price_unit;
-    }
-    size_t get_units()
-    {
-        return units;
-    }
     virtual std::string get_name() = 0;
-
-    double price()
-    {
-        return price_unit * units;
-    }
+    double get_price_unit() { return price_unit; }
+    size_t get_units() { return units; }
+    double price() { return price_unit * units; }
 
 protected:
     Ingredient(double price_unit, size_t units)
@@ -27,10 +17,9 @@ protected:
         , units { units }
     {
     }
-
+    std::string name;
     double price_unit;
     size_t units;
-    std::string name;
 };
 
 #endif // INGREDIENT_H

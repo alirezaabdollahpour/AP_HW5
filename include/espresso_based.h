5317c6708d2715176859bbd5a15124ff9035fadd
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-05-22 17:51:15 +0430

    espresso_based.h implement for GUI

--- include/espresso_based.h
+++ include/espresso_based.h
@@ -3,15 +3,27 @@
 
 #include "ingredient.h"
 #include "sub_ingredients.h"
+#include <random>
 #include <string>
+#include <thread>
 #include <vector>
+// libraries that we need for EspressoBased::brew
+#include "ftxui/dom/node.hpp" // for Render
+#include "ftxui/screen/color.hpp" // for ftxui
+#include <chrono> // for operator""s, chrono_literals
+#include <cmath> // for cos
+#include <ftxui/dom/elements.hpp> // for Fit, canvas, operator|, border, Element
+#include <ftxui/screen/screen.hpp> // for Pixel, Screen
+#include <iostream> // for cout, endl, ostream
+#include <string> // for allocator, operator+, char_traits, operator<<, string, to_string, basic_string
+#include <thread> // for sleep_for
 
 class EspressoBased {
 public:
     virtual std::string get_name() = 0;
     virtual double price() = 0;
 
-    // void brew();
+    void brew();
     std::vector<Ingredient*>& get_ingredients();
 
     virtual ~EspressoBased();

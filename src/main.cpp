78491cf0b0685d99a53ec7ca8c6604d76cc1b5a2
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-05-22 18:15:03 +0430

    src/main.cpp is clean

--- src/main.cpp
+++ src/main.cpp
@@ -8,39 +8,6 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-#include <ftxui/dom/elements.hpp>
-#include <ftxui/screen/screen.hpp>
-#include <iostream>
-
-// int main(void)
-// {
-//     using namespace ftxui;
-
-//     // Define the document
-//     // Element document = hbox({
-//     //     text("left") | border,
-//     //     text("middle") | border | flex,
-//     //     text("right") | border,
-//     // });
-//     Element document = vbox({ text("The window") | bold | color(Color::Blue),
-//         gauge(0.5)
-//             text("The footer") });
-
-//     // Add a border, by calling the `ftxui::border` decorator function.
-//     document = border(document);
-
-//     // Add another border, using the pipe operator.
-//     document = document | border.
-
-//                           // Add another border, using the |= operator.
-//                           document
-//         |= border
-//             Render(screen, document);
-//     screen.Print();
-
-//     return EXIT_SUCCESS;
-// }
-
 int main(int argc, char** argv)
 {
     if (false) // make false to run unit-tests

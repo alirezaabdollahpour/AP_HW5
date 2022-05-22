1e969c831dc4a1310c9b28c3530b0712e9910dec
alirezaabdollahpour <alirezaabdollahpour1380@gmail.com>
2022-05-22 17:57:35 +0430

    cleaning comments espresso_based.h

--- include/espresso_based.h
+++ include/espresso_based.h
@@ -1,22 +1,18 @@
 #ifndef ESPRESSO_BASED_H
 #define ESPRESSO_BASED_H
 
+#include "ftxui/dom/node.hpp"
+#include "ftxui/screen/color.hpp"
 #include "ingredient.h"
 #include "sub_ingredients.h"
-#include <random>
+#include <chrono>
+#include <cmath>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <iostream>
 #include <string>
 #include <thread>
 #include <vector>
-// libraries that we need for EspressoBased::brew
-#include "ftxui/dom/node.hpp" // for Render
-#include "ftxui/screen/color.hpp" // for ftxui
-#include <chrono> // for operator""s, chrono_literals
-#include <cmath> // for cos
-#include <ftxui/dom/elements.hpp> // for Fit, canvas, operator|, border, Element
-#include <ftxui/screen/screen.hpp> // for Pixel, Screen
-#include <iostream> // for cout, endl, ostream
-#include <string> // for allocator, operator+, char_traits, operator<<, string, to_string, basic_string
-#include <thread> // for sleep_for
 
 class EspressoBased {
 public:
